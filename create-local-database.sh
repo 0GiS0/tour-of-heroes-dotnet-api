a476a9bd069c907444bb106ac9c99ee9822f39a9
0gis0 <giselatb@outlook.com>
2021-11-17 12:23:42 +0100

    Added support for Apple M1, updated heroes info

--- create-local-database.sh
+++ create-local-database.sh
@@ -9,4 +9,23 @@ docker run \
 # Create database 
 docker exec -it sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Password1!' -Q 'CREATE DATABASE [heroes]'
 
+# Apple M1
+
+#Create a network
+docker network create sqlserver-vnet
+
+#Create a container with Azure SQL Edge
+docker run \
+--name azuresqledge \
+--network sqlserver-vnet \
+--cap-add SYS_PTRACE -e 'ACCEPT_EULA=1' \
+-e 'MSSQL_SA_PASSWORD=Password1!' \
+-p 1433:1433 \
+-d mcr.microsoft.com/azure-sql-edge
+
+#Create a database (this step is not necessary)
+docker run -it --network sqlserver-vnet mcr.microsoft.com/mssql-tools
+sqlcmd -S azuresqledge -U SA -P 'Password1!' -Q 'CREATE DATABASE [heroes]'
+
 #and just execute the Web API using this database
+#Connection string: Server=localhost,1433;Initial Catalog=heroes;Persist Security Info=False;User ID=sa;Password=Password1!;
