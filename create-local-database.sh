af9e66bb4532e2cacb8ed85fb767ac34d84841cd
0gis0 <giselatb@outlook.com>
2021-11-21 10:38:48 +0100

    add volume to the database in order to keep changes if i delete the db

--- create-local-database.sh
+++ create-local-database.sh
@@ -1,13 +1,11 @@
-# Create local database on Docker
+# Create local database on Docker Mac/Linux
 docker run \
 -e 'ACCEPT_EULA=Y' \
 -e 'SA_PASSWORD=Password1!' \
 -e 'MSSQL_PID=Express' \
 --name sqlserver \
+ -v mssqlserver_volume:/var/opt/mssql \
 -p 1433:1433 -d mcr.microsoft.com/mssql/server:latest
-
-# Create database 
-docker exec -it sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Password1!' -Q 'CREATE DATABASE [heroes]'
 
 # Apple M1
 
@@ -20,12 +18,18 @@ docker run \
 --network sqlserver-vnet \
 --cap-add SYS_PTRACE -e 'ACCEPT_EULA=1' \
 -e 'MSSQL_SA_PASSWORD=Password1!' \
+ -v mssqlserver_volume:/var/opt/mssql \
 -p 1433:1433 \
 -d mcr.microsoft.com/azure-sql-edge
 
-#Create a database (this step is not necessary)
-docker run -it --network sqlserver-vnet mcr.microsoft.com/mssql-tools
-sqlcmd -S azuresqledge -U SA -P 'Password1!' -Q 'CREATE DATABASE [heroes]'
+# Create local database on Windows (PowerShell)
+docker run `
+-e 'ACCEPT_EULA=Y' `
+-e 'SA_PASSWORD=Password1!' `
+-e 'MSSQL_PID=Express' `
+--name sqlserver `
+ -v mssqlserver_volume:/var/opt/mssql `
+-p 1433:1433 -d mcr.microsoft.com/mssql/server:latest
 
 #and just execute the Web API using this database
 #Connection string: Server=localhost,1433;Initial Catalog=heroes;Persist Security Info=False;User ID=sa;Password=Password1!;
