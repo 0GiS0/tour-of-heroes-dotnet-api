93853dabdf6eecee2d163b2ec40baf79974c4510
0gis0 <giselatb@outlook.com>
2021-12-15 21:31:07 +0100

    add App Service as a Code

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,28 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = "Tour-Of-Heroes"
-    location = "North Europe"
+  name     = "Tour-Of-Heroes"
+  location = "North Europe"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "tour-of-heroes-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "web" {
+  name = "tour-of-heroes-web-api"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+
+  
+
 }
