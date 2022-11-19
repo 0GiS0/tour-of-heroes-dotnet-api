1e408c6592fa3c02b1d5509766bfea2d67bcb74b
Gisela <giselatb@outlook.com>
2022-11-19 11:07:17 +0100

    delete terraform commands

--- create-local-database.sh
+++ create-local-database.sh
@@ -33,23 +33,3 @@ docker run `
 
 #and just execute the Web API using this database
 #Connection string: Server=localhost,1433;Initial Catalog=heroes;Persist Security Info=False;User ID=sa;Password=Password1!;
-
-# Terraform
-terraform init \
--backend-config="storage_account_name=tfstateslemon" \
--backend-config="container_name=demo" \
--backend-config="access_key=82U256laUIzzWyCJ2DfC9rhgAoO4JlIvE7xNB0VcfwaDMI6LbFca94QYRA3no3yzRiCuGE9itlwHhdSlVf81wA=="
-
-terraform plan -out=tfplan
-
-terraform apply tfplan \
--backend-config="storage_account_name=${secrets.AZURE_STORAGE_ACCOUNT_NAME}" \
--backend-config="container_name=${secrets.AZURE_STORAGE_CONTAINER_NAME}" \
--backend-config="access_key=${secrets.AZURE_STORAGE_ACCESS_KEY}"
-
-az login
-
-AZURE_SUBSCRIPTION_ID=$(az account show --query id -o tsv)
-
-# Create a service principal
-az ad sp create-for-rbac --name "terraform-sp" --role contributor --scopes "/subscriptions/$AZURE_SUBSCRIPTION_ID"
