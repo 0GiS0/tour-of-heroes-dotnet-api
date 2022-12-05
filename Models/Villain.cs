99bb60e53c1f86cb521cc8df57ff2b0567817747
Gisela <giselatb@outlook.com>
2022-12-05 10:41:32 +0100

    add http requests to fill the db

--- Models/Villain.cs
+++ Models/Villain.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace tour_of_heroes_api.Models
 {
     public class Villain
     {
-        public int Id { get; set; }
+        [Key]
+        public int VillainId { get; set; }
+        [Required]
         public string Name { get; set; }
+        public int HeroId { get; set; }
+        public Hero? Hero { get; set; }
         public string Description { get; set; }
     }
 }
