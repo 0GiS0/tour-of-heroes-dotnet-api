7aa1156ea9230f477097a6f9cfe0e58aa9621898
0GiS0 <giselatb@outlook.com>
2023-03-11 22:06:25 +0000

    update terraform types

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_resource_group" "rg" {
 
 
 # Create a Azure SQL Server 
-resource "azurerm_sql_server" "sqlserver" {
+resource "azurerm_mssql_server" "sqlserver" {
   name                         = "heroserver"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
@@ -39,64 +39,73 @@ resource "azurerm_sql_server" "sqlserver" {
 }
 
 # Allow Azure services and resources to access this server
-resource "azurerm_sql_firewall_rule" "sqlserver" {
-  name                = "allow-azure-services"
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_sql_server.sqlserver.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
+resource "azurerm_mssql_firewall_rule" "sqlserver" {
+  name             = "allow-azure-services"
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
 
 # Create a database
-resource "azurerm_sql_database" "sqldatabase" {
-  name                = "heroes"
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_sql_server.sqlserver.name
-  location            = azurerm_resource_group.rg.location
-  edition             = "Basic"
+resource "azurerm_mssql_database" "sqldatabase" {
+  name      = "heroes"
+  server_id = azurerm_mssql_server.sqlserver.id
+  sku_name  = "Basic"
 }
 
 # Azure App Service Plan
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   name                = "tour-of-heroes-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  # sku {
+  #   tier = "Standard"
+  #   size = "S1"
+  # }
+
+  os_type = "Windows"
+
+  sku_name = "S1"
 }
 
 # Create Web App
-resource "azurerm_app_service" "web" {
+resource "azurerm_windows_web_app" "web" {
   name                = "tour-of-heroes-webapi"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+  service_plan_id = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      current_stack  = "dotnetcore"
+      dotnet_version = "v7.0"
+    }
+  }
 
   # Connection Strings
   connection_string {
     name  = "DefaultConnection"
-    value = "Server=tcp:${azurerm_sql_server.sqlserver.name}.database.windows.net,1433;Initial Catalog=${azurerm_sql_database.sqldatabase.name};Persist Security Info=False;User ID=${var.db_user};Password=${var.db_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+    value = "Server=tcp:${azurerm_mssql_server.sqlserver.name}.database.windows.net,1433;Initial Catalog=${azurerm_mssql_database.sqldatabase.name};Persist Security Info=False;User ID=${var.db_user};Password=${var.db_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
     type  = "SQLAzure"
   }
 }
 
 # Create Web App slot
-resource "azurerm_app_service_slot" "web" {
-  name                = "staging"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_windows_web_app_slot" "web" {
+  name = "staging"
 
-  app_service_plan_id = azurerm_app_service_plan.plan.id
-  app_service_name    = azurerm_app_service.web.name
+  app_service_id = azurerm_windows_web_app.web.id
 
   # Connection Strings
   connection_string {
     name  = "DefaultConnection"
-    value = "Server=tcp:${azurerm_sql_server.sqlserver.name}.database.windows.net,1433;Initial Catalog=${azurerm_sql_database.sqldatabase.name};Persist Security Info=False;User ID=${var.db_user};Password=${var.db_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+    value = "Server=tcp:${azurerm_mssql_server.sqlserver.name}.database.windows.net,1433;Initial Catalog=${azurerm_mssql_database.sqldatabase.name};Persist Security Info=False;User ID=${var.db_user};Password=${var.db_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
     type  = "SQLAzure"
+  }
+
+  site_config {
+    
   }
 }
