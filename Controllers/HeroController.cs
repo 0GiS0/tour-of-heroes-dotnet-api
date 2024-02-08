61b0a8a21b022dce98e79cc880aff44d4464d2c8
0GiS0 <giselatb@outlook.com>
2024-02-08 23:48:50 +0100

    Fix namespace typo and add HeroRepository and IHeroRepository

--- Controllers/HeroController.cs
+++ Controllers/HeroController.cs
@@ -1,14 +1,10 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Azure.Storage.Blobs;
-using Azure.Storage.Sas;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using tour_of_heroes_api.Models;
-using tour_of_heroes_api.Modesl;
+using System.Linq;
+using Azure.Storage.Blobs;
+
 
 namespace tour_of_heroes_api.Controllers
 {
@@ -16,102 +12,84 @@ namespace tour_of_heroes_api.Controllers
     [ApiController]
     public class HeroController : ControllerBase
     {
-        private readonly HeroContext _context;
 
-        public HeroController(HeroContext context)
+        private IHeroRepository _heroRepository;
+        public HeroController(IHeroRepository heroRepository)
         {
-            _context = context;
+            _heroRepository = heroRepository;
         }
 
         // GET: api/Hero
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Hero>>> GetHeroes()
+        public ActionResult<IEnumerable<Hero>> GetHeroes()
         {
-            return await _context.Heroes.ToListAsync();
+            var heroes = _heroRepository.GetAll();
+            return Ok(heroes);
         }
 
         // GET: api/Hero/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Hero>> GetHero(int id)
+        public ActionResult<Hero> GetHero(int id)
         {
-            var hero = await _context.Heroes.FindAsync(id);
+            var hero = _heroRepository.GetById(id);
 
             if (hero == null)
             {
                 return NotFound();
             }
 
-            return hero;
+            return Ok(hero);
         }
 
         // PUT: api/Hero/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutHero(int id, Hero hero)
+        public ActionResult PutHero(int id, Hero hero)
         {
-            if (id != hero.Id)
+
+            var heroToUpdate = _heroRepository.GetById(id);
+
+            if (heroToUpdate == null)
             {
-                return BadRequest();
+                return NotFound();
             }
 
-            _context.Entry(hero).State = EntityState.Modified;
+            heroToUpdate.Name = hero.Name;
+            heroToUpdate.AlterEgo = hero.AlterEgo;
+            heroToUpdate.Description = hero.Description;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!HeroExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            _heroRepository.Update(heroToUpdate);
 
             return NoContent();
+
         }
 
         // POST: api/Hero
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Hero>> PostHero(Hero hero)
+        public ActionResult<Hero> PostHero(Hero hero)
         {
-            _context.Heroes.Add(hero);
-            await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetHero), new { id = hero.Id }, hero);
+            _heroRepository.Add(hero);
+
+            return Ok(hero);
+
         }
 
         // DELETE: api/Hero/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteHero(int id)
+        public IActionResult DeleteHero(int id)
         {
-            var hero = await _context.Heroes.FindAsync(id);
-            if (hero == null)
-            {
-                return NotFound();
-            }
-
-            _context.Heroes.Remove(hero);
-            await _context.SaveChangesAsync();
+            _heroRepository.Delete(id);
 
             return NoContent();
-        }
-
-        private bool HeroExists(int id)
-        {
-            return _context.Heroes.Any(e => e.Id == id);
         }
 
         // GET: api/hero/alteregopic/5
         [HttpGet("alteregopic/{id}")]
         public async Task<ActionResult<Hero>> GetAlterEgoPic(int id)
         {
-            var hero = await _context.Heroes.FirstOrDefaultAsync(h => h.Id == id);
+            var hero = _heroRepository.GetById(id);
 
             if (hero == null)
             {
@@ -135,40 +113,6 @@ namespace tour_of_heroes_api.Controllers
 
             //return image
             return File(image.Value.Content, "image/png");
-        }
-
-        // GET: api/hero/alteregopic/sas
-        [HttpGet("alteregopic/sas/{imgName}")]
-        public ActionResult GetAlterEgoPicSas(string imgName)
-        {
-            //Get image from Azure Storage
-            string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
-            
-            // Create a BlobServiceClient object which will be used to create a container client
-            var blobServiceClient = new BlobServiceClient(connectionString);
-
-            //Get container client
-            var containerClient = blobServiceClient.GetBlobContainerClient("alteregos");
-
-            //Get blob client
-            var blobClient = containerClient.GetBlobClient(imgName);
-
-            var sasBuilder = new BlobSasBuilder()
-            {
-                BlobContainerName = "alteregos",
-                BlobName = imgName,
-                Resource = "b"
-            };
-
-            sasBuilder.ExpiresOn = DateTimeOffset.UtcNow.AddMinutes(3);
-            sasBuilder.SetPermissions(BlobSasPermissions.Read | BlobSasPermissions.Write);
-
-            Uri sasUri = blobClient.GenerateSasUri(sasBuilder);
-
-            Console.WriteLine($"SAS Uri for blob is: {sasUri}");
-
-            //return image
-            return Ok($"{blobServiceClient.Uri}{sasUri.Query.ToString()}");
         }
     }
 }
